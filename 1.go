94ad2a8d7c8c052981c6a5329f404e4db9bc186e
07Roshi <121103694+07Roshi@users.noreply.github.com>
2023-06-10 09:56:48 +0000

    update

---
+++ 1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var prev, current, sum int
+	prev = 0
+	current = 1
+	sum = 0
+	for prev+current < 40 {
+		next := prev + current
+		fmt.Println(next)
+		if next%2 == 0 {
+			sum += next
+		}
+		prev = current
+		current = next
+	}
+	fmt.Println("The sum of the even-valued terms is ", sum)
+}
---
+++ 2.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+import "math"
+
+func largestPrimeFactor(n int) int{
+    largest :=1
+    
+    for n%2 ==0{
+        largest = 2
+        n /= 2
+    }
+    for i := 3; i <= int(math.Sqrt(float64(n))); i += 2{
+        for n%i ==0{
+            largest = i
+            n /= i
+        }
+    }
+    if n > 2{
+        largest = n
+    }
+    return largest
+}
+func main(){
+    number := 14
+    largest := largestPrimeFactor(number)
+    fmt.Println("The largest prime factor of %d is %d\n",number,largest)
+}
