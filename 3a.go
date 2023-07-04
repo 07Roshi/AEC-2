9310df6da667054257a573cb09dd3234b0528a6c
07Roshi <121103694+07Roshi@users.noreply.github.com>
2023-07-04 13:32:54 +0000

    update

---
+++ 3a.go
@@ -0,0 +1,36 @@
+package main
+import "fmt"
+import "math"
+
+type Shape interface{
+	Area() float64
+}
+
+type Circle struct{
+	radius float64
+}
+
+func (c Circle) Area() float64{
+	return math.Pi*c.radius*c.radius
+}
+
+type Rectangle struct{
+	width float64
+	height float64
+}
+
+func (r Rectangle) Area() float64{
+	return r.width*r.height
+}
+
+func PrintArea(s Shape){
+	fmt.Printf("Area: %0.2f\n",s.Area())
+}
+
+func main(){
+	circle := Circle{radius: 5}
+	rectangle := Rectangle{width: 4, height: 6}
+
+	PrintArea(circle)
+	PrintArea(rectangle)
+}
